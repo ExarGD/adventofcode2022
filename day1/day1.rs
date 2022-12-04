df497b25974c3f1a08b0140bb211e66215ee4dbe
Adel Mubarakshin <adel.mubarakshin@paradoxinteractive.com>
2022-12-04 18:15:10 +0100

    Day 1: Task 1 solved

--- day1/day1.rs
+++ day1/day1.rs
@@ -1,4 +1,23 @@
-fn main() {
-    println!("Hello, day1!");
-}
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
 
+fn main() -> io::Result<()> {
+    let file = File::open("day1/input.txt")?;
+    let reader = BufReader::new(file);
+    let mut count = 0;
+    let mut max = 0;
+    for line in reader.lines().map(|l| l.unwrap()) {
+        if line.parse::<String>().unwrap() != "" {
+            count += line.parse::<i32>().unwrap_or(0);
+            // println!("{}", count);
+        } else {
+            if count > max {
+                max = count;
+            }
+            count = 0;
+       }
+    }
+    println!("{}", max);
+
+    Ok(())
+}
