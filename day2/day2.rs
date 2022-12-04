f6ce182810b552afdf1020374dd77dec4baf5ed0
Adel Mubarakshin <adel.mubarakshin@paradoxinteractive.com>
2022-12-04 20:43:56 +0100

    Day 2: Task 2 solved

--- day2/day2.rs
+++ day2/day2.rs
@@ -6,6 +6,11 @@ A X: Rock 1
 B Y: Paper 2
 C Z: Scissors 3
 L 1, D 3, W 6
+
+t2:
+X = loose
+Y = draw
+Z = win
 */
 
 fn main() -> io::Result<()> {
@@ -14,16 +19,30 @@ fn main() -> io::Result<()> {
     let mut score: i32;
     let mut count: i32 = 0;
     for line in reader.lines().map(|l| l.unwrap()) {
+        // Task 1
+        // match line.as_str() {
+        //     "A X" => score = 1 + 3,
+        //     "A Y" => score = 2 + 6,
+        //     "A Z" => score = 3 + 0,
+        //     "B X" => score = 1 + 0,
+        //     "B Y" => score = 2 + 3,
+        //     "B Z" => score = 3 + 6,
+        //     "C X" => score = 1 + 6,
+        //     "C Y" => score = 2 + 0,
+        //     "C Z" => score = 3 + 3,
+        //     _ => score = 0,
+        // };
+        // Task 2
         match line.as_str() {
-            "A X" => score = 1 + 3,
-            "A Y" => score = 2 + 6,
-            "A Z" => score = 3 + 0,
+            "A X" => score = 3 + 0,
+            "A Y" => score = 1 + 3,
+            "A Z" => score = 2 + 6,
             "B X" => score = 1 + 0,
             "B Y" => score = 2 + 3,
             "B Z" => score = 3 + 6,
-            "C X" => score = 1 + 6,
-            "C Y" => score = 2 + 0,
-            "C Z" => score = 3 + 3,
+            "C X" => score = 2 + 0,
+            "C Y" => score = 3 + 3,
+            "C Z" => score = 1 + 6,
             _ => score = 0,
         };
         count += score;
