4d56aa2db47c299b30e008addd49362001aaabfa
Adel Mubarakshin <adel.mubarakshin@paradoxinteractive.com>
2022-12-04 19:05:48 +0100

    Day 1: Task 2 solved

--- day1/day1.rs
+++ day1/day1.rs
@@ -5,19 +5,18 @@ fn main() -> io::Result<()> {
     let file = File::open("day1/input.txt")?;
     let reader = BufReader::new(file);
     let mut count = 0;
-    let mut max = 0;
+    let mut vec: Vec<i32> = Vec::new();
     for line in reader.lines().map(|l| l.unwrap()) {
         if line.parse::<String>().unwrap() != "" {
             count += line.parse::<i32>().unwrap_or(0);
-            // println!("{}", count);
         } else {
-            if count > max {
-                max = count;
-            }
+            vec.push(count);
             count = 0;
        }
     }
-    println!("{}", max);
+    vec.sort();
+    vec.reverse();
+    println!("{}", vec[0] + vec[1] + vec[2]);
 
     Ok(())
 }
